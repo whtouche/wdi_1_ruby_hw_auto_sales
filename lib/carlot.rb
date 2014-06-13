8b3fcccc95a2f3b1ebc140f760f010de5b84cc61
Ben White <whtouche@gmail.com>
2014-06-13 08:55:09 -0400

    Link carlot with damage and usedcard

---
+++ lib/carlot.rb
@@ -0,0 +1,19 @@
+require_relative 'car'
+require_relative 'used_car'
+class CarLot
+
+
+  def initialize(name)
+    @name = name
+  end
+
+end
+
+=begin
+### `CarLot`
+#B Do last
+* has a name
+* may have one or more cars
+* can retrieve the "lot value" (total price of all cars on the lot)
+* can retrieve a list of all cars matching a certain make, model, or year
+=end
