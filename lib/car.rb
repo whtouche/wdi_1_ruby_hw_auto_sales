88a96fbeaa76470ea2b344ae5129ffb5f293ae8b
Ben White <whtouche@gmail.com>
2014-06-12 23:33:46 -0400

    Create car class
    *add methods to calculate age and value of car

---
+++ lib/car.rb
@@ -0,0 +1,41 @@
+require 'date'
+class Car
+
+
+  def initialize(make, model, year, msrp)
+    @make = make
+    @model = model
+    @year = year
+    @msrp = msrp
+  end
+
+  def age
+    Time.now.strftime("%Y").to_i - @year
+  end
+
+  def value # Straight line dep. with min of 0
+    if @msrp - ((@msrp * 0.05) * age) < 0
+      0
+    else @msrp - ((@msrp * 0.05) * age)
+    end
+
+  end
+
+  def price
+    value + markup
+  end
+
+end
+
+=begin
+B-Do first, is overclass!
+* has a make, model, and year of manufacture
+#B-Shelly = Car.new('Ford', 'Fiesta', 1984, 5000)
+* has an MSRP (original retail price)
+#B-Above
+* has a value (MSRP modified by depreciation, taking the year of manufacture into account)
+#B-Function w/in class Car. Use date calculation from person.rb, require 'date'
+* all cars have a constant depreciation of 5% per year
+* has a manager markup
+* has a price (value modified by the manager markup)
+=end
